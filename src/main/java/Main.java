fa8b46b8eb828177f67c2443ad96b802edda42f3
Shenno Willaert <shenno.w@gmail.com>
2016-10-07 15:23:21 +0200

    find me

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String txt = "";
+        for(int i=0; i<6; i++){txt += txt .length() == 0 ? "h" : txt .length() == 2 ? "j" : i == 3 || i == 4 ? (txt .charAt(txt .length()-1) == ' ' ? "d" : " ") : i == 5 ? "å" : "e";}
+        System.out.printLn(txt);
+    }
+}
